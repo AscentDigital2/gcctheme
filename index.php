d9ad904da583cb3f6a552a4b9d89bb127285e798
xydfayt <xydfayt@yahoo.com>
2018-03-01 18:59:29 +0800

    Sponsors

--- index.php
+++ index.php
@@ -51,7 +51,7 @@
 	        		$query->the_post();
 	        		$thumb_url = wp_get_attachment_image_src(get_post_thumbnail_id(), '', false);
 	    ?>
-        <div class="col-md-2 col-xs-6 col-sm-6 sponsor-box"><a href="<?php the_field('url'); ?>"><img src="<?php echo $thumb_url[0]; ?>" alt=""></a></div>
+        <div class="col-md-2 col-xs-6 col-sm-6 sponsor-box"><a href="<?php the_field('url'); ?>"><img src="<?php echo $thumb_url[0]; ?>" class ="sponsor-img" alt=""></a></div>
         <?php
         		}
         	}
