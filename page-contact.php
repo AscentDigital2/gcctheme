f5d7d0e4877666c9109c8c0ad266b89a70e9c1f0
Stephan Sutter <stephan.sutter2000@gmail.com>
2018-03-09 12:49:28 +0800

    Fixed officer list

--- page-contact.php
+++ page-contact.php
@@ -56,10 +56,12 @@
                 );
                 $query = new WP_Query($args);
                 if($query->have_posts()){
-                  $query->the_post();
+                  while($query->have_posts()){
+                    $query->the_post();
         ?>
-        <div class="col-md-4 col-xs-4 advisor-box"><p class="o-name"><?php echo get_the_title(); ?></p><p class="o-title"><?php echo get_field('position'); ?></p><p class="o-email"><?php echo get_field('email'); ?></p></div>
+        <div class="col-md-4 col-xs-12 advisor-box"><p class="o-name"><?php echo get_the_title(); ?></p><p class="o-title"><?php echo get_field('position'); ?></p><p class="o-email"><?php echo get_field('email'); ?></p></div>
       <?php
+              }
             }
             wp_reset_query();
       ?>
